a1accadcebcf408a4a1f13b2dc8d69d8f9fa573c
Moshe <moisherosenzweig@gmail.com>
2022-01-30 11:21:09 -0500

    WWTLLIIWE

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+// alternative user names
+const currentUser = 'Grace Hopper';
+
+const currentUserExcited = 'GRACE HOPPER';
+
+const initial = "G";
+
+// Messages to user
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUserExcited} + !`
+
+const shortGreeting = "Welcome, " + initial + '!';
